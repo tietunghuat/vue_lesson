718585cadd95c66d55be304f73012fd4111ddfb3
Tie Tung Huat <tunghuat.2000@gmail.com>
2022-09-08 22:56:26 +0800

    first commit

---
+++ my-first-class/src/views/HomeView.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="home">
+    <HelloWorld msg="Welcome to Your Vue.js App!!" />
+
+    <div id="home1">
+      <textarea class="ii1" placeholder="Show Here..."></textarea>
+    </div>
+  </div>
+</template>
+<script>
+import HelloWorld from "@/components/HelloWorld.vue";
+export default {
+  name: "HomeView",
+  components: { HelloWorld }
+};
+</script>
+
+<style>
+textarea {
+  width: 250px;
+  height: 350px;
+  border: grey 1px solid;
+  margin-bottom: 15px;
+}
+input{
+  width:250px
+}
+</style>
