d7473223fe2816dd8c0de49439a9f863902b1d43
Tie Tung Huat <tunghuat.2000@gmail.com>
2022-10-10 00:37:44 +0800

    10/9

--- project-two-class/src/App.vue
+++ project-two-class/src/App.vue
@@ -1,18 +1,56 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="app">
+    <!-- <p>{{name}} = {{age}}</p>
+  <button @click="change('Hello')">Change name</button>
+  <button @click="changeage(30)">Change age</button> -->
+
+  <p>{{face[0].name}}</p>
+  </div>
+  
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import { defineComponent,reactive,toRefs,ref } from 'vue';
+import facehere from "./type/type"
 
 export default defineComponent({
   name: 'App',
-  components: {
-    HelloWorld
+  components: {},
+  // setup(){
+  //  const state = reactive({
+  //   name:"hihihi",
+  //   age:25 as number | string
+
+  //  })
+  //  state.name="hahaha",
+  //  state.age=33
+  //  return {...toRefs(state)}
+   
+  // },
+  // setup(){
+  //   const name=ref("link")
+  //   const age=ref<string|number>(39)
+  //   return {name,age}
+  // },,
+  setup(){
+    const face=ref<facehere[]>([
+      {id:1,name:"King",age:23,over_age:true},
+  {id:2,name:"Kingaa",age:26,over_age:true}
+])
+  return {face}
   }
+//   ,
+//   methods:{
+//     change(name:string){  
+//       this.name= name;
+
+//     },
+//     changeage(age:string | number){
+//       this.age=age;
+//     }
+//   }
 });
+
 </script>
 
 <style>
---
+++ project-two-class/src/type/type.ts
@@ -0,0 +1,7 @@
+interface facehere{
+    readonly id:number,
+    name:string,
+    age:number,
+    over_age:boolean
+}
+export default facehere
